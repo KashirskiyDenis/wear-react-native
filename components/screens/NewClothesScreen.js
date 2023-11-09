f080ce6dd1551f0c3333873035658508c19eb324
KashirskiyDenis <waka_flocka@mail.ru>
2023-11-09 16:45:26 +0400

    Add files via upload

--- components/screens/NewClothesScreen.js
+++ components/screens/NewClothesScreen.js
@@ -3,39 +3,49 @@ import {
   Button,
   Dimensions,
   Image,
+  ImageBackground,
   StyleSheet,
-  Text,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import AddButton from '../AddButton';
 
 const WIDTH = Dimensions.get('window').width;
 
 function AddClothesScreen({ navigation }) {
   const [image, setImage] = useState(null);
+  const [height, setHeight] = useState(null);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
     });
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+      let coef = result.assets[0].width / WIDTH;
+      setHeight(parseInt(result.assets[0].height / coef));
     }
+  };
+
+  const onPress = (event) => {
+    let coordX = event.nativeEvent.locationX;
+    let coordY = event.nativeEvent.locationY;
   };
 
   return (
     <View style={{ flex: 1 }}>
-      <Text>Add new clothes Screen</Text>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && (
-        <Image source={{ uri: image }} style={{ width: WIDTH, height: WIDTH }} />
+        <TouchableWithoutFeedback
+          onPress={onPress}
+          style={{ width: WIDTH, height: height }}>
+          <Image
+            source={{ uri: image }}
+            resizeMode="contain"
+            style={{ width: WIDTH, height: height }}></Image>
+        </TouchableWithoutFeedback>
       )}
     </View>
   );
